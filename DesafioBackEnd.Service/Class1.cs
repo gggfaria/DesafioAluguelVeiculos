08a506cd3534c92ed446d6beff99185e89799647
Gabriel Faria <gggfaria@outlook.com>
2024-04-20 17:02:02 -0300

    Adicionando projeto service e projeto test

---
+++ DesafioBackEnd.Service/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DesafioBackEnd.Service;
+
+public class Class1
+{
+}
---
+++ DesafioBackEnd.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace DesafioBackEnd.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
